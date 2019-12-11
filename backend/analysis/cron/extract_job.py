eeacf1845a92267c6ca5632f312239c586b6ab94
Jelte van Boheemen <j.vanboheemen@gmail.com>
2019-12-11 11:47:56 +0100

    extract cron-job working again

--- backend/analysis/cron/extract_job.py
+++ backend/analysis/cron/extract_job.py
@@ -1,8 +1,9 @@
-import os, errno
+import os
+import errno
 from shutil import copyfile
 from django_cron import CronJobBase, Schedule
 from zipfile import ZipFile
-from ..models import File
+from ..models import UploadFile
 
 
 class ExtractJob(CronJobBase):
@@ -12,8 +13,7 @@ class ExtractJob(CronJobBase):
     code = 'sasta.extract_job'  # a unique code
 
     def do(self):
-        for file in File.objects.filter(status="pending"):
-            print(file.content.name)
+        for file in UploadFile.objects.filter(status="pending"):
             try:
                 self.extract(file)
             except Exception as error:
@@ -22,24 +22,26 @@ class ExtractJob(CronJobBase):
     def extract(self, file):
         file.status = 'extracting'
         file.save()
+
         try:
-            (dir, filename) = os.path.split(file.content.name)
-            target_dir = dir.replace("uploads", "extracted")
-            print(target_dir)
+            (origin_dir, filename) = os.path.split(file.content.name)
+            target_dir = origin_dir.replace('uploads', 'extracted')
+            # extract zipped files
             if filename.lower().endswith(".zip"):
-                with ZipFile(file.content) as zip:
-                    zip.extractall(target_dir)
+                with ZipFile(file.content) as zipfile:
+                    zipfile.extractall(target_dir)
+            # copy all others
             else:
-                # copy file
                 try:
                     os.makedirs(target_dir)
                 except OSError as e:
                     if e.errno != errno.EEXIST:
                         raise
                 copyfile(file.content.name, os.path.join(target_dir, filename))
-
+                print(os.path.join(target_dir, filename))
             file.status = 'extracted'
             file.save()
+
         except:
             file.status = 'extraction-failed'
             file.save()
