5a4d16700dc3124e2e8b61ab278564ce44ec4bf2
Jelte van Boheemen <j.vanboheemen@gmail.com>
2019-12-16 14:09:09 +0100

    OOP rewrite

--- backend/analysis/convert/CHAT_converter.py
+++ backend/analysis/convert/CHAT_converter.py
@@ -1,29 +1,146 @@
 import errno
 import os
 import re
+from typing import List, Optional, Union, Pattern
 
 AGE_FIELD_NAMES = ['age', 'leeftijd']
 SEX_FIELD_NAMES = ['sex', 'gender', 'geslacht']
+# TODO handle corpus name/transcript title
+TITLE_FIELD_NAMES = ['samplename', 'title', 'titel']
 MALE_CODES = ['jongen', 'man', 'boy', 'man']
 FEMALE_CODES = ['meisje', 'vrouw', 'girl', 'woman']
 
 
-class CHATConverter:
-    def __init__(self, file, outfile):
-        self.input_path = file
-        self.output_path = outfile
+def match_pattern(pattern: Pattern, line: str):
+    match = re.match(pattern, line)
+    if not match:
+        return None
+    return match.groups()
 
-        self.transcript_header = '@UTF-8\n@Begin\n@Languages:\tnld\n'
-        self.transcript_content = ''
-        self.transcript_footer = '@End'
-        self.metadata = []
-        self.metadata_coms = '\n'
-        self.participants = []
-        self.ids = ''
 
-        # TODO multiple target speakers
-        self.target_speaker = {'code': None, 'age': '',
-                               'sex': '', 'role': 'Target_Adult'}
+class Participant:
+    def __init__(self, code: str,
+                 age: Optional[str] = None, sex: Optional[str] = None,
+                 role: Optional[str] = None, target_speaker=False):
+        self.code = code
+        self.age = age
+        self.sex = sex
+        self.role = role
+        self.target_speaker = target_speaker
+
+    def role_from_age(self) -> str:
+        age_pattern = re.compile(r'(\d+);(\d{2})?(?:(?:\.)(\d{2}))?')
+
+        if self.age:
+            match = age_pattern.match(self.age)
+            if match:
+                years = int(match.group(1))
+                return 'Target_Child' if years < 18 else 'Target_Adult'
+        return 'Other'
+        # try:
+        #     match = age_pattern.match(self.age)
+        #     years = int(match.group(1))
+        #     return 'Target_Child' if years < 18 else 'Target_Adult'
+        # except:
+        #     return 'Other'
+
+    @property
+    def id_header(self) -> str:
+        return f'@ID:\tnld||{self.code}|{self.age or ""}|{self.sex or ""}|||{self.role or self.role_from_age()}|||'
+
+    @property
+    def participant_header(self) -> str:
+        return f'{self.code} {self.code.lower()} {self.role or self.role_from_age()}'
+
+
+class Utterance:
+    def __init__(self, speaker_code: str, text: str, utt_id: Optional[int] = None):
+        self.speaker_code = speaker_code
+        self.utt_id = utt_id
+        self.text = text
+
+    def __str__(self):
+        if self.utt_id:
+            return f'*{self.speaker_code}:\t{self.text}\n%xsid:\t{self.utt_id}'
+        return f'*{self.speaker_code}:\t{self.text}'
+
+
+class Tier:
+    def __init__(self, code: str, value: str):
+        self.code = code
+        self.value = value
+
+    def __str__(self):
+        return f'{self.code}\t{self.value}'
+
+
+class MetaValue:
+    def __init__(self, field_type: str, key: str, value: Union[str, int]):
+        self.field_type = field_type
+        self.key = key
+        self.value = value.strip() if isinstance(value, str) else value
+
+
+class MetaComment(MetaValue):
+    #  Metadata that is copied as a comment to the CHAT format
+    # def __init__(self, *args, **kwargs):
+    #     super().__init__(*args, **kwargs)
+
+    def __str__(self):
+        return f'@Comment ##META {self.field_type} {self.key} = {self.value}'
+
+
+class SifDocument:
+    def __init__(self,
+                 participants: List[Participant],
+                 content: List[Union[Utterance, Tier]],
+                 meta_comments: List[MetaComment]):
+        self.participants = participants
+        self.content = content
+        self.meta_comments = meta_comments
+
+    def write_chat(self, out_file_path: str):
+        output_dir = os.path.dirname(out_file_path)
+        try:
+            os.makedirs(output_dir)
+        except OSError as exc:
+            if exc.errno == errno.EEXIST and os.path.isdir(output_dir):
+                pass
+            else:
+                raise
+
+        with open(out_file_path, 'w', encoding='utf-8') as file:
+            # File headers
+            print('@UTF8', '@Begin', '@Languages:\tnld', sep='\n', file=file)
+            # @Participants header
+            print(
+                f'@Participants:\t{", ".join([p.participant_header for p in self.participants])}', file=file)
+            # @ID headers
+            print(*[p.id_header for p in self.participants], sep='\n', file=file)
+            # Metadata comments
+            print(*self.meta_comments, sep='\n', file=file)
+            print('\r', file=file)
+            # Content (utterances and tiers)
+            print(*self.content, sep='\n', file=file)
+            # Footer
+            print('@End', file=file)
+
+
+class SifReader:
+    def __init__(self, in_file_path: str):
+        self.file_path = in_file_path
+
+        self.participants: List[Participant] = []
+        self.content: List[Union[Utterance, Tier]] = []
+        self.metadata: List[MetaValue] = []
+        self.meta_comments: List[MetaComment] = []
+
+        self.read()
+        self.parse_metadata()
+
+    @property
+    def document(self):
+        return SifDocument(self.participants, self.content, self.meta_comments)
 
     @property
     def patterns(self):
@@ -34,7 +151,7 @@ class CHATConverter:
         # groups:   1) tier code    2) value
         tier_pattern = r'^(%\w{3,4}):\s*(.*)$'
         # groups:   1) speaker code
-        single_speaker_pattern = r'^##TARGET\sSPEAKERS\s=\s([A-Z]{3})$'
+        single_speaker_pattern = r'^##TARGET\sSPEAKER(?:S)?\s=\s([A-Z]{3})$'
         # no groups
         target_uttids_pattern = r'^##TARGET\sUTTIDS$'
 
@@ -43,106 +160,55 @@ class CHATConverter:
             tier_pattern, single_speaker_pattern,
             target_uttids_pattern]]
 
-    def read(self):
-        with open(self.input_path, 'r') as file:
-            for line in file.readlines():
-                [meta, utt, tier, single_spk, _tar_uttids] = [self.match_pattern(
-                    pattern, line) for pattern in self.patterns]
-                if meta:
-                    dtype, field, value = meta
-                    self.metadata.append({'dtype': dtype,
-                                          'field': field, 'value': value})
-                elif utt:
-                    self.parse_utt(utt)
-                elif tier:
-                    self.transcript_content += line
-                elif single_spk:
-                    self.target_speaker['code'] = single_spk[0]
-                else:
-                    pass
+    def parse_utterance(self, utterance):
+        utt_id, code, text = utterance
+        known_participant_codes = [p.code for p in self.participants]
+        if code not in known_participant_codes:
+            self.participants.append(Participant(code))
 
-        self.parse()
-        self.write()
-
-    def parse(self):
-        meta = self.parse_metadata()
-        self.make_participant_ids(self.target_speaker, self.participants)
-        if meta:
-            self.transcript_header += f'\n{"".join(meta)}\n'
-
-        print(self.transcript_header)
-        print(self.transcript_content)
-        print(self.transcript_footer)
-
-    def write(self):
-        output_dir = os.path.dirname(self.output_path)
-        try:
-            os.makedirs(output_dir)
-        except OSError as exc:
-            if exc.errno == errno.EEXIST and os.path.isdir(output_dir):
-                pass
-            else:
-                raise
-
-        with open(self.output_path, 'w', encoding='utf-8') as file:
-            file.write(self.transcript_header)
-            file.write(self.transcript_content)
-            file.write(self.transcript_footer)
-
-    def match_pattern(self, pattern, line):
-        match = re.match(pattern, line)
-        if not match:
-            return None
-        else:
-            return match.groups()
-
-    def parse_utt(self, utterance):
-        u_id, spk, utt = utterance
-        if spk not in self.participants:
-            self.participants.append(spk)
-        out = f'*{spk}:\t{utt}\n'
-        if u_id:
-            out += f'%xuid:\t{u_id}\n'
-        self.transcript_content += out
+        self.content.append(Utterance(code, text, utt_id))
 
     def parse_metadata(self):
-        # groups:   1) years    2) months   3) days
-        age_pattern = re.compile(r'(\d+);(\d{2})?(?:(?:\.)(\d{2}))?')
-        metadata_coms = []
+        ''' Metadata pertaining to target speaker'''
+        target_speaker = [
+            p for p in self.participants if p.target_speaker][0] or None
         for meta in self.metadata:
-            field_type, field, value = meta['dtype'], meta['field'], meta['value']
-
-            if field.lower() in AGE_FIELD_NAMES:
-                self.target_speaker['age'] = value
-                years = int(age_pattern.match(value).group(1))
-                self.target_speaker['role'] = 'Target_Child' if years < 18 else 'Target_Adult'
-
-            elif field.lower() in SEX_FIELD_NAMES:
-                if value.lower() in MALE_CODES:
-                    self.target_speaker['sex'] = 'male'
-                elif value.lower() in FEMALE_CODES:
-                    self.target_speaker['sex'] = 'female'
+            # parse age
+            if meta.key.lower() in AGE_FIELD_NAMES:
+                target_speaker.age = meta.value
+            # parse sex
+            elif meta.key.lower() in SEX_FIELD_NAMES:
+                if meta.value.lower() in MALE_CODES:
+                    target_speaker.sex = 'male'
+                elif meta.value.lower() in FEMALE_CODES:
+                    target_speaker.sex = 'female'
                 else:
-                    self.target_speaker['sex'] = 'unknown'
+                    target_speaker.sex = 'unknown'
 
-            else:
-                metadata_coms += f'@Comment ##META {field_type} {field} = {value}'
+    def read(self):
+        with open(self.file_path, 'r') as file:
+            for line in file.readlines():
+                [meta, utt, tier, single_spk, _tar_uttids] = [match_pattern(
+                    pattern, line) for pattern in self.patterns]
 
-        return metadata_coms
+                if meta:
+                    if meta[1] in AGE_FIELD_NAMES or meta[1] in SEX_FIELD_NAMES:
+                        self.metadata.append(
+                            MetaValue(*meta))
+                    else:
+                        self.meta_comments.append(MetaComment(*meta))
+                elif utt:
+                    self.parse_utterance(utt)
+                elif tier:
+                    self.content.append(Tier(*tier))
+                elif single_spk:
+                    self.participants.append(Participant(
+                        *single_spk, target_speaker=True))
 
-    def make_participant_ids(self, tar_speaker, other_speakers):
-        # TODO implement name/role translations for commonly used speaker codes
-        speakers = [
-            f'{tar_speaker["code"]} {tar_speaker["code"].lower()} {tar_speaker["role"]}']
-        for code in [spk for spk in other_speakers if spk != tar_speaker['code']]:
-            speakers.append(f'{code} {code.lower()} Other')
 
-        participants_header = f'@Participants:\t{", ".join(speakers)}\n'
+def test():
+    sf = SifReader('ASTA_01.txt')
+    sf.document.write_chat('/Users/3248526/Documents/ASTA_01.cha')
 
-        target_speaker_id = f'@ID:\tnld||{tar_speaker["code"]}|{self.target_speaker["age"]}|{tar_speaker["sex"]}|||{tar_speaker["role"]}|||\n'
-        participant_ids = [f'@ID:\tnld||{code}|||||Other|||\n' for code in [
-            p for p in other_speakers if p != tar_speaker['code']]]
 
-        self.transcript_header += participants_header
-        self.transcript_header += target_speaker_id
-        self.transcript_header += ''.join(participant_ids)
+test()
