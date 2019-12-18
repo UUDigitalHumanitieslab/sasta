f26f52a75eed05aa7e40864a70a6f5c248c27b6f
Jelte van Boheemen <j.vanboheemen@gmail.com>
2019-12-18 13:40:38 +0100

    remove transcript functionality from frontend

--- frontend/src/app/store.ts
+++ frontend/src/app/store.ts
@@ -1,14 +1,11 @@
 import { ActionReducer } from '@ngrx/store';
 import { CorpusEffects } from './store/corpus.effects';
 import { corpusReducer } from './store/corpus.reducer';
-import { TranscriptsEffects } from './store/transcripts.effects';
-import { transcriptsReducer } from './store/transcripts.reducer';
 import { uploadFilesReducer } from './store/upload-files.reducer';
 import { UploadFilesEffects } from './store/upload-files.effects';
 
 
 export const reducers = {
-    transcripts: transcriptsReducer,
     corpora: corpusReducer,
     uploadFiles: uploadFilesReducer
 };
@@ -19,4 +16,4 @@ export type storeStructure = {
     [K in keyof typeof reducers]: ActionType<(typeof reducers)[K]>;
 };
 
-export const effects = [TranscriptsEffects, CorpusEffects, UploadFilesEffects];
+export const effects = [CorpusEffects, UploadFilesEffects];
