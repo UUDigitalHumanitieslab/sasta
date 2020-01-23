21e471b3aa556f520208a6e14875c04351706a9b
Jelte van Boheemen <j.vanboheemen@gmail.com>
2020-01-23 15:27:57 +0100

    attempt logging

--- backend/analysis/cron/parse_job.py
+++ backend/analysis/cron/parse_job.py
@@ -1,10 +1,15 @@
-from ..models import Transcript
-from django_cron import CronJobBase, Schedule
-from corpus2alpino.converter import Converter
+import os
+
 from corpus2alpino.annotators.alpino import AlpinoAnnotator
 from corpus2alpino.collectors.filesystem import FilesystemCollector
+from corpus2alpino.converter import Converter
+from corpus2alpino.log import Log, LogSingleton, LogTarget
+from corpus2alpino.targets.filesystem import FilesystemTarget
 from corpus2alpino.targets.memory import MemoryTarget
 from corpus2alpino.writers.lassy import LassyWriter
+from django_cron import CronJobBase, Schedule
+
+from ..models import Transcript
 
 
 class ParseJob(CronJobBase):
@@ -15,15 +20,47 @@ class ParseJob(CronJobBase):
 
     def do(self):
         print('Running ParseJob')
+        # for transcript in Transcript.objects.filter(name="Stap_01_TranscriptWord"):
         for transcript in Transcript.objects.all():
-            print(transcript.content.path)
+            try:
+                self.parse_transcript(transcript)
+            except Exception as e:
+                print('klapt op do()')
+                print(e)
+
+    def parse_transcript(self, transcript):
+        transcript.status = 'parsing'
+        transcript.save()
+
+        try:
+            transcript_dir, _ = os.path.splitext(transcript.content.path)
+            os.makedirs(transcript_dir, exist_ok=True)
+            print(transcript.name)
+
             alpino = AlpinoAnnotator("localhost", 7001)
+
+            # log_singleton = LogSingleton
+            # log_target = LogTarget(target=FilesystemTarget(
+            #     'logs'))
+            # log = Log(log_target, strict=False)
+            # log_singleton.set(log)
+
             converter = Converter(
                 collector=FilesystemCollector([transcript.content.path]),
                 annotators=[alpino],
-                target=MemoryTarget(),
-                writer=LassyWriter(True)
+                target=FilesystemTarget(transcript_dir),
+                # target=MemoryTarget(),
+                writer=LassyWriter(False),
             )
             parses = converter.convert()
-            print(''.join(parses))
-            break
+            for parse in parses:
+                print(parse)
+            transcript.status = 'parsed'
+            transcript.save()
+            print('----')
+
+        except Exception as e:
+            print('parse klapt')
+            print(e)
+            transcript.status = 'parsing-failed'
+            transcript.save()
