cd3d22dcea7cd2b7bf46a41731a1bb8a7bec241d
Jelte van Boheemen <j.vanboheemen@gmail.com>
2020-02-26 10:49:09 +0100

    simple log for parsing

--- backend/analysis/cron/parse_job.py
+++ backend/analysis/cron/parse_job.py
@@ -5,7 +5,6 @@ from corpus2alpino.collectors.filesystem import FilesystemCollector
 from corpus2alpino.converter import Converter
 from corpus2alpino.log import Log, LogSingleton, LogTarget
 from corpus2alpino.targets.filesystem import FilesystemTarget
-from corpus2alpino.targets.memory import MemoryTarget
 from corpus2alpino.writers.lassy import LassyWriter
 from django_cron import CronJobBase, Schedule
 
@@ -19,9 +18,10 @@ class ParseJob(CronJobBase):
     code = 'sasta.parse_job'  # a unique code
 
     def do(self):
-        print('Running ParseJob')
-        # for transcript in Transcript.objects.filter(name="Stap_01_TranscriptWord"):
-        for transcript in Transcript.objects.all():
+        LogSingleton.set(Log(LogTarget(target=FilesystemTarget(
+            'logs')), strict=True))
+
+        for transcript in Transcript.objects.filter(status='converted'):
             try:
                 self.parse_transcript(transcript)
             except Exception as e:
@@ -33,34 +33,27 @@ class ParseJob(CronJobBase):
         transcript.save()
 
         try:
+            LogSingleton.get().warning(f'Parsing:\t{transcript.name}\n')
             transcript_dir, _ = os.path.splitext(transcript.content.path)
             os.makedirs(transcript_dir, exist_ok=True)
-            print(transcript.name)
 
             alpino = AlpinoAnnotator("localhost", 7001)
-
-            # log_singleton = LogSingleton
-            # log_target = LogTarget(target=FilesystemTarget(
-            #     'logs'))
-            # log = Log(log_target, strict=False)
-            # log_singleton.set(log)
 
             converter = Converter(
                 collector=FilesystemCollector([transcript.content.path]),
                 annotators=[alpino],
                 target=FilesystemTarget(transcript_dir),
-                # target=MemoryTarget(),
                 writer=LassyWriter(False),
             )
             parses = converter.convert()
-            for parse in parses:
-                print(parse)
+            for _parse in parses:
+                LogSingleton.get().warning(
+                    f'Succesfully parsed:\t{transcript.name}')
             transcript.status = 'parsed'
             transcript.save()
-            print('----')
 
         except Exception as e:
-            print('parse klapt')
-            print(e)
+            LogSingleton.get().error(
+                f'Error parsing "{transcript.name}" with message:\t{e}')
             transcript.status = 'parsing-failed'
             transcript.save()
