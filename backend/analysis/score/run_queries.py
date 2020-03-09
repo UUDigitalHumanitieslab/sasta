8e75e159203c8faa405018027cbf98a3ef50975d
Jelte van Boheemen <j.vanboheemen@uu.nl>
2020-03-09 15:41:17 +0100

    transcript results by utt and by query

--- backend/analysis/score/run_queries.py
+++ backend/analysis/score/run_queries.py
@@ -26,12 +26,15 @@ def compile_xpath_or_func(query: str) -> Union[ET.XPath, None]:
         return None
 
 
-def query_transcript(transcript: Transcript, method: AssessmentMethod, phase=2, phase_exact=True):
-    queries = filter_queries(method, phase, phase_exact=False)
+def query_transcript(transcript: Transcript, method: AssessmentMethod, phase=None, phase_exact=False):
+    queries = filter_queries(method, phase, phase_exact)
     queries_with_funcs = compile_queries(queries)
 
-    transcript_results = query_single_transcript(
+    by_utterance_results = query_single_transcript(
         transcript, queries_with_funcs)
+    by_query_results = results_by_query(by_utterance_results)
+
+    return by_utterance_results, by_query_results
 
 
 def filter_queries(method: AssessmentMethod, phase: int = None, phase_exact: bool = True):
@@ -77,38 +80,65 @@ def query_single_transcript(transcript: Transcript, queries_with_funcs):
         with open(transcript.parsed_content.path, 'rb') as f_in:
             doc = ET.fromstring(f_in.read())
             utterances = doc.xpath('.//alpino_ds')
-            # aggregation of results for entire transcript
+            # aggregation of results for entire transcript, grouped on utterance
             transcript_results = OrderedDict({
                 'name': transcript.name,
                 'utterances': []
             })
 
-            with open('logs/score.log', 'w') as f_out:
-                for utt in utterances:
-                    copied_utt = deepcopy(utt)
-                    sent = copied_utt.xpath('sentence')[
-                        0].text.replace('\n', '')
-                    xsid = copied_utt.xpath(
-                        '//meta[@name="xsid"]')[0].attrib['value']
+            for utt in utterances:
+                copied_utt = deepcopy(utt)
+                sent = copied_utt.xpath('sentence')[
+                    0].text.replace('\n', '')
+                xsid = copied_utt.xpath(
+                    '//meta[@name="xsid"]')[0].attrib['value']
 
-                    # results for a single utterance
-                    utterance_result = OrderedDict({
-                        'utt_id': xsid,
-                        'sentence': sent,
-                        'hits': []
-                    })
+                # results for a single utterance
+                utterance_result = OrderedDict({
+                    'utt_id': xsid,
+                    'sentence': sent,
+                    'hits': []
+                })
 
-                    for query in queries_with_funcs:
-                        query_results = run_single_query(
-                            query['q_func'], copied_utt)
-                        if query_results:
-                            utterance_result['hits'].append((
-                                query['q_id'], len(query_results)))
-                    if utterance_result['hits']:
-                        transcript_results['utterances'].append(
-                            utterance_result)
+                for query in queries_with_funcs:
+                    query_results = run_single_query(
+                        query['q_func'], copied_utt)
+                    if query_results:
+                        utterance_result['hits'].append((
+                            query['q_id'], len(query_results)))
+                if utterance_result['hits']:
+                    transcript_results['utterances'].append(
+                        utterance_result)
 
-            return transcript_results
+        return transcript_results
+
     except Exception as e:
         print(f'error querying:\t{e}')
         return None
+
+
+def results_by_query(input_results):
+    # results for a transcript, grouped by query
+    transcript_results = input_results.copy()
+    by_utt_results = transcript_results.pop('utterances', None)
+    transcript_results['queries'] = []
+
+    query_results = {}
+
+    for item in by_utt_results:
+        # OrderedDict([('utt_id', '2'), ('sentence', 'innu kast'), ('hits', [('T147', 1)])])
+        # query_results.append((item['utt_id']))
+        for hit in item['hits']:
+            if not hit[0] in query_results:
+                query_results[hit[0]] = []
+            query_results[hit[0]].append(
+                (item['utt_id'], item['sentence'], hit[1]))
+
+    for k in sorted(query_results.keys()):
+        transcript_results['queries'].append(
+            OrderedDict({
+                'query_id': k,
+                'hits': query_results[k]
+            })
+        )
+    return(transcript_results)
