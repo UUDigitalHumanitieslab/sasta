018fd20750b91d17a8cf5b1e2959556a92a47a25
Jelte van Boheemen <j.vanboheemen@uu.nl>
2020-03-19 14:05:58 +0100

    remove old docx2txt

--- backend/analysis/utils.py
+++ backend/analysis/utils.py
@@ -68,20 +68,6 @@ def docx_to_txt(filepath):
         print('error in docx_to_txt:\t', error)
 
 
-# def docx_to_txt(filepath):
-#     '''converts .docx file to .txt file'''
-#     try:
-#         document = Document(filepath)
-#         txt_path = filepath.replace('.docx', '.txt')
-#         with open(txt_path, 'w', encoding='utf-8') as txt_file:
-#             for para in document.paragraphs:
-#                 print(para.text, file=txt_file)
-#         os.remove(filepath)
-#         return txt_path
-#     except Exception as error:
-#         print(error)
-
-
 def read_TAM(method) -> None:
     filepath = method.content.path
     logger.info(f'TAM-Reader:\treading {os.path.basename(filepath)}')
