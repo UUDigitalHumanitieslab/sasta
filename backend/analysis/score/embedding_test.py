bcbb96cca078a39d19444e63b0b7b4d964520383
Jelte van Boheemen <j.vanboheemen@uu.nl>
2020-06-16 14:49:36 +0200

    ZC embedding algorithm

---
+++ backend/analysis/score/embedding_test.py
@@ -0,0 +1,17 @@
+import os.path as op
+
+import pytest
+from lxml import etree as ET
+
+from .zc_embedding import get_zc_embeddings
+
+HERE = op.dirname(op.abspath(__file__))
+FILES = op.join(HERE, 'testfiles')
+
+
+def test_zc_embed():
+    tree = ET.parse(op.join(FILES, 'zc_embed_test.xml'))
+    expected_embeddings = [1]*5 + [2]*4
+    word_indices = [str(x) for x in range(0, 9)]
+    expected = dict(zip(word_indices, expected_embeddings))
+    assert expected == get_zc_embeddings(tree)
---
+++ backend/analysis/score/zc_embedding.py
@@ -0,0 +1,40 @@
+
+from typing import Dict
+
+
+def is_token(node):
+    return any(x in ['pt', 'pos'] for x in node.keys())
+
+
+def has_cat(node):
+    return 'cat' in node.keys()
+
+
+def is_clausal(node):
+    return is_direct_clausal(node) or is_child_clausal(node)
+
+
+def is_direct_clausal(node):
+    return node.attrib['cat'] in ['smain', 'rel', 'whrel', 'whsub', 'whq', 'sv1']
+
+
+def is_child_clausal(node):
+    return node.attrib['cat'] in ['cp'] and \
+        any(n.attrib['cat'] in ['ssub', 'ti'] for n in list(node))
+
+
+def solve(node, embed, results):
+    if is_token(node):
+        results[node.attrib['begin']] = embed
+    if has_cat(node) and is_clausal(node):
+        embed += 1
+    for child in node.getchildren():
+        solve(child, embed, results)
+    return results
+
+
+def get_zc_embeddings(syntree) -> Dict[str, int]:
+    root = syntree.getroot()
+    top_node = root[0]
+    results = solve(top_node, 0, {})
+    return results
