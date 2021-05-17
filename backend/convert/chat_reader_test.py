42b03a88362444554a8f740160f0ea46a7dbffdd
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-05-17 09:59:25 +0200

    cleanup chat tests

--- backend/convert/chat_reader_test.py
+++ backend/convert/chat_reader_test.py
@@ -1,9 +1,7 @@
-import os.path as op
 from .chat_reader import ChatDocument
 
 
-def test_chat_reader():
-    here = op.dirname(op.abspath(__file__))
-    fp = op.join(here, 'testfiles', 'tt_example.cha')
-    doc = ChatDocument.from_chatfile(fp)
-    assert len(doc.lines) == 3
+def test_chat_reader(chafiles):
+    for (input, _) in chafiles:
+        doc = ChatDocument.from_chatfile(input)
+        assert len(doc.lines)
--- backend/convert/chat_writer_test.py
+++ backend/convert/chat_writer_test.py
@@ -1,21 +1,12 @@
-import os.path as op
-import os
 from .chat_reader import ChatDocument
 from .chat_writer import ChatWriter
 
 
-def test_chat_writer():
-    here = op.dirname(op.abspath(__file__))
-    fp = op.join(here, 'testfiles', 'tt_example.cha')
-    doc = ChatDocument.from_chatfile(fp)
-    assert len(doc.lines) == 3
-
-    out_fp = op.join(here, 'testfiles', 'tt_example_out.cha')
-
-    with open(out_fp, 'w+') as target:
-        writer = ChatWriter(doc, target=target)
-        writer.write()
-
-    out_doc = ChatDocument.from_chatfile(out_fp)
-    assert out_doc == doc
-    os.remove(out_fp)
+def test_chat_writer(chafiles):
+    for inpath, outpath in chafiles:
+        doc = ChatDocument.from_chatfile(inpath)
+        with open(outpath, 'w+', encoding='utf-8') as target:
+            writer = ChatWriter(doc, target=target)
+            writer.write()
+        out_doc = ChatDocument.from_chatfile(outpath)
+        assert out_doc == doc
