5a7d1b9713e243d618aad8660724dd6dff3909c9
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-06-04 13:20:17 +0200

    change celery settings

--- backend/sasta/celery.py
+++ backend/sasta/celery.py
@@ -1,23 +1,24 @@
-from __future__ import absolute_import
 import os
 from celery import Celery
-from django.conf import settings
 
-import logging
-logger = logging.getLogger('sasta')
 
-# set the default Django settings module for the 'celery' program.
-# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sasta.settings')
+# set the Django settings module for the 'celery' program.
+# if no environment variable is set yet, set it to the settings next to this file.
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', os.environ.get(
+    'DJANGO_SETTINGS_MODULE', 'sasta.settings'))
+
 app = Celery('sasta')
-#  broker=settings.CELERY_BROKER_URL,
-#              backend=settings.CELERY_BACKEND)
-logger.info(settings.CELERY_BROKER_URL)
-# Using a string here means the worker will not have to
-# pickle the object when using Windows.
-app.config_from_object('django.conf:settings')
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+# Using a string here means the worker doesn't have to serialize
+# the configuration object to child processes.
+# - namespace='CELERY' means all celery-related configuration keys
+#   should have a `CELERY_` prefix.
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Load task modules from all registered Django app configs.
+app.autodiscover_tasks()
 
 
 @app.task(bind=True)
 def debug_task(self):
-    print('Request: {0!r}'.format(self.request))
+    print(f'Request: {self.request!r}')
