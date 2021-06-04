473a93852a42f2fd047d400f075f27057182e1fb
Jelte van Boheemen <j.vanboheemen@uu.nl>
2021-06-04 13:10:25 +0200

    no default settings

--- backend/sasta/celery.py
+++ backend/sasta/celery.py
@@ -3,12 +3,15 @@ import os
 from celery import Celery
 from django.conf import settings
 
+import logging
+logger = logging.getLogger('sasta')
+
 # set the default Django settings module for the 'celery' program.
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sasta.settings')
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sasta.settings')
 app = Celery('sasta')
 #  broker=settings.CELERY_BROKER_URL,
 #              backend=settings.CELERY_BACKEND)
-
+logger.info(settings.CELERY_BROKER_URL)
 # Using a string here means the worker will not have to
 # pickle the object when using Windows.
 app.config_from_object('django.conf:settings')
