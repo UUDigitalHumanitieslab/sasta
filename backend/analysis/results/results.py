75720c7ae8b5b7d68398be139e39a860596bee6a
Jelte van Boheemen <j.vanboheemen@uu.nl>
2022-03-30 10:05:25 +0200

    Annotate allresults

--- backend/analysis/results/results.py
+++ backend/analysis/results/results.py
@@ -22,18 +22,44 @@ class UtteranceWord:
         return self.__str__()
 
 
-QueryTuple = Tuple[str, str]
-SynTree = Union[ET._Element, str]
-SastaMatch = Tuple[Match, SynTree]
+QueryTuple = Tuple[str, str]  # (query_id, utt_id)
+SynTree = Union[ET._Element, str]  # lxml etree or string representation
+SastaMatch = Tuple[Match, SynTree]  # (matched part of the tree, whole tree)
 SastaMatchList = List[SastaMatch]
 SastaMatches = Dict[QueryTuple, SastaMatchList]
-SastaResults = Dict[str, Counter[str]]
+SastaResults = Dict[str, Counter[str]]  # {query_id: Counter({utt_id: num_matches, ..}, ..)
 SastaAnnotations = Dict[str, List[UtteranceWord]]
-# Exact results: {query_id: [(utt_id, match_word_begin+1), (utt_id, match_word_begin+1)]}
-SastaExactResults = Dict[str, List[Tuple[Union[str, int], int]]]
+SastaExactResults = Dict[str, List[Tuple[Union[str, int], int]]]  # exactresults: {query_id: [(utt_id, match_word_begin+1), ..], ..}
+SastaAllUtts = Dict[str, List[str]]  # allutts : {utt_id: ['word1', 'word2', ..], ..}
 
 
 class AllResults:
+    """Query results and annotations of a single transcript
+
+    Attributes
+    ----------
+    filename: str
+        Transcript name. Does not need to be a complete filepath.
+    uttcount: int
+        Total number of utterances.
+    coreresults: dict
+        Results of pre and core queries, grouped by query.
+    exactresults: dict
+        Results, grouped by query
+    postresults: dict
+        Results of post queries, grouped by query.
+    allmatches: dict
+        All matched queries with the matched xml nodes.
+    annotations: dict
+        All annotations for the transcript, grouped by utterance.
+    analysedtrees: list
+        Syntactic trees (xml) for all utterances.
+    annotationinput: bool
+        Read annotations from SAF-file rather then generate from transcript.
+    allutts: dict
+        Dictionary with keys str(utt_id) and values list of words
+    """
+
     def __init__(self,
                  filename,
                  uttcount,
@@ -54,10 +80,10 @@ class AllResults:
         self.annotations: SastaAnnotations = annotations or {}
         self.analysedtrees: List[SynTree] = analysedtrees or []
         self.annotationinput: bool = annotationinput or False
-        self.allutts: List = alluts or None
+        self.allutts: SastaAllUtts = alluts or None
 
     def __repr__(self):
-        return f'name: {self.filename}, core: {len(self.coreresults)}, post: {len(self.postresults)}, matches: {len(self.allmatches)}'  # noqa: E501
+        return f'name: {self.filename}, core: {len(self.coreresults)}, post: {len(self.postresults)}, matches: {len(self.allmatches)}'
 
 
 def scores2counts(scores):
