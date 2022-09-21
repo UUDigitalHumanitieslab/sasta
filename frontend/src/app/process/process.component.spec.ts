025d88e95eba7bef02422ddd5bd3a5ec4c23c916
Jelte van Boheemen <j.vanboheemen@uu.nl>
2022-09-21 08:36:16 +0200

    Process single transcript

--- frontend/src/app/process/process.component.spec.ts
+++ frontend/src/app/process/process.component.spec.ts
@@ -2,32 +2,42 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
+import { MessageService } from 'primeng/api';
 import { StepsModule } from 'primeng/steps';
+import { ParseService } from '../services/parse.service';
 
 import { ProcessComponent } from './process.component';
 import { TranscriptProgressCellComponent } from './transcript-progress-cell.component';
 import { TranscriptProgressComponent } from './transcript-progress.component';
 
 describe('ProcessComponent', () => {
-  let component: ProcessComponent;
-  let fixture: ComponentFixture<ProcessComponent>;
+    let component: ProcessComponent;
+    let fixture: ComponentFixture<ProcessComponent>;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
-      declarations: [ProcessComponent, TranscriptProgressComponent, TranscriptProgressCellComponent],
-      imports: [StepsModule, RouterTestingModule, HttpClientTestingModule],
-      schemas: [CUSTOM_ELEMENTS_SCHEMA]
-    })
-    .compileComponents();
-  }));
+    beforeEach(waitForAsync(() => {
+        TestBed.configureTestingModule({
+            declarations: [
+                ProcessComponent,
+                TranscriptProgressComponent,
+                TranscriptProgressCellComponent,
+            ],
+            providers: [ParseService, MessageService],
+            imports: [
+                StepsModule,
+                RouterTestingModule,
+                HttpClientTestingModule,
+            ],
+            schemas: [CUSTOM_ELEMENTS_SCHEMA],
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(ProcessComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(ProcessComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
